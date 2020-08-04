9a02b64c860c2ddbc0c9f964bb075e93609fc952
Kenneth Neary <kennethneary@gmail.com>
2020-08-04 14:14:27 +0100

    add old project

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: '',
+    authDomain: '.firebaseapp.com',
+    databaseURL: '.firebaseio.com',
+    projectId: 'ng-fitness-tracker',
+    storageBucket: '',
+    messagingSenderId: ''
+  }
+};
